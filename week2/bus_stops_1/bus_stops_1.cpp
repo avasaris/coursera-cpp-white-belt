cf246e50140ea715653c6bbd8de5be62cd0085fc
avasaris <artvas@yandex.ru>
2019-04-15 09:31:12 +0200

    add command BUSES_FOR_STOP

--- week2/bus_stops_1/bus_stops_1.cpp
+++ week2/bus_stops_1/bus_stops_1.cpp
@@ -1,7 +1,4 @@
-﻿// bus_stops_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include "pch.h"
+﻿#include "pch.h"
 #include <iostream>
 #include <string>
 #include <vector>
@@ -9,13 +6,11 @@
 
 using namespace std;
 
-
-
-
 int main()
 {
 	vector<string> buses_order;
 	map<string, vector<string>> buses;
+	map<string, vector<string>> stops;
 
 	int q;
 	cin >> q;
@@ -24,18 +19,35 @@ int main()
 		string operation_code;
 		cin >> operation_code;
 
-
-
 		if (operation_code == "NEW_BUS") {
 			
 			string new_bus;
-			int stops;
-			cin >> new_bus >> stops;
+			int stop_cnt;
+			cin >> new_bus >> stop_cnt;
 
 			buses_order.push_back(new_bus);
-			buses[new_bus].resize(stops);
 
-			for (auto &stop : buses[new_bus]) cin >> stop;
+			for (; stop_cnt-- > 0;) {
+				string new_stop;
+				cin >> new_stop;
+				buses[new_bus].push_back(new_stop);
+				stops[new_stop].push_back(new_bus);
+			}
+		}
+
+		else if (operation_code == "BUSES_FOR_STOP") {
+
+			string stop;
+			cin >> stop;
+
+			if (stops.find(stop) != stops.end()) {
+				for (const auto & bus : stops[stop]) cout << bus << " ";
+				cout << endl;
+			}
+			else {
+				cout << "No stop\n";
+			}
+
 		}
 
 		else if (operation_code == "ALL_BUSES") {
@@ -54,19 +66,12 @@ int main()
 			}
 		}
 
+		else {
+			cout << "\nError: Unrecognized input\n";
+			return 1;
+		}
 		
 	}
 
-    
+	return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.

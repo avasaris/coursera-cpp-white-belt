63810b0bbd771b3fbc81076c13691cea1f8fca4a
avasaris <artvas@yandex.ru>
2019-04-15 15:48:41 +0200

    New task "Synonyms"

---
+++ week2/Synonyms_1/Synonyms_1.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+
+using namespace std;
+
+int main()
+{
+	map<string, set<string>> synonyms;
+
+	int q;
+	cin >> q;
+
+	while (q--) {
+
+		string operation_code, word1, word2;
+		cin >> operation_code;
+
+		if (operation_code == "ADD") {
+			cin >> word1 >> word2;
+			synonyms[word1].insert(word2);
+			synonyms[word2].insert(word1);
+		}
+		else if (operation_code == "COUNT") {
+			cin >> word1;
+			cout << synonyms[word1].size() << endl;
+		}
+		else if (operation_code == "CHECK") {
+			cin >> word1 >> word2;
+			size_t syn_count = synonyms[word1].count(word2);
+			if (syn_count == 0) {
+				cout << "NO" << endl;
+			}
+			else {
+				cout << "YES" << endl;
+			}
+		}
+
+	}
+	return 0;
+}

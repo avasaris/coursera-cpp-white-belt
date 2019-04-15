637c2a3ebb49cd193b5281f154913b8affe4d391
avasaris <artvas@yandex.ru>
2019-04-15 15:57:19 +0200

    New task "Bus stops 3"

---
+++ week2/bus_stops_3/bus_stops_3.cpp
@@ -0,0 +1,36 @@
+﻿#include "pch.h"
+
+using namespace std;
+
+int main()
+{
+	map<set<string>, int> bus_paths;
+
+	int q;
+	cin >> q;
+
+	while (q--) {
+
+		int n;
+		cin >> n;
+
+		set<string> path;
+
+		while (n--) {
+			string stop;
+			cin >> stop;
+			path.insert(stop);
+		}
+
+		if (bus_paths.find(path) == bus_paths.end()) {
+			int new_num = bus_paths.size() + 1;
+			bus_paths[path] = new_num;
+			cout << "New bus " << new_num << endl;
+		}
+		else {
+			cout << "Already exists for " << bus_paths[path] << endl;
+		}
+
+	}
+	return 0;
+}

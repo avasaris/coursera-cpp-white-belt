0afc407660524d2d17e3dd27cab6d0b9470704f1
avasaris <artvas@yandex.ru>
2019-04-15 11:27:34 +0200

    New task init

---
+++ week2/bus_stops_2/bus_stops_2.cpp
@@ -0,0 +1,34 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main()
+{
+	map<string, int> bus_paths;
+
+	int q;
+	cin >> q;
+
+	while (q--) {
+
+		int n;
+		cin >> n;
+
+		string path;
+		getline(cin, path);
+
+		if (bus_paths.find(path) != bus_paths.end()) {
+			int new_num = bus_paths.size();
+			bus_paths[path] = new_num;
+			cout << "New bus " << new_num << endl;
+		}
+		else {
+			cout << "Already exists for " << bus_paths[path] << endl;
+		}
+
+	}
+	return 0;
+}
